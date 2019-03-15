6a9cf421f561037aa55b4b3a306889c32848baad
Tristamoff <site.include@gmail.com>
2019-03-15 17:33:56 +0300

    Add save button

---
+++ Block/Adminhtml/SaveButton.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Magentotest\Tasks\Block\Adminhtml;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+/**
+ * Class SaveButton
+ * @package Magentotest\Tasks\Block\Adminhtml
+ */
+class SaveButton extends GenericButton implements ButtonProviderInterface
+{
+    /**
+     * @return array
+     */
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Save Task'),
+            'class' => 'save primary',
+            'data_attribute' => [
+                'mage-init' => ['button' => ['event' => 'save']],
+                'form-role' => 'save',
+            ],
+            'sort_order' => 90,
+        ];
+    }
+}
---
+++ Block/Adminhtml/TasksList.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Magentotest\Tasks\Block\Adminhtml;
+
+/**
+ * Class TasksList
+ * @package Magentotest\Tasks\Block\Adminhtml
+ */
+class TasksList extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+    {
+        parent::__construct($context);
+    }
+}
